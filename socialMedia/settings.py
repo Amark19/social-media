59cd1061252bc9a7d02e49aa570d7e24e635785a
Amar K <71604396+Amark19@users.noreply.github.com>
2022-03-23 00:45:03 +0530

    updated settings.py

--- socialMedia/settings.py
+++ socialMedia/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '6i(tk1o*w#vp@@rnrsh=b7i-@xhly(ncmxk$dzl14c!_0i=-f2'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
