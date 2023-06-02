223fea91c3a3bd5cc6c09d078387af5814787949
Amark19 <amarkhamkar6@gmail.com>
2023-06-02 14:29:03 +0530

    Done with followers,following feature .

--- user/models.py
+++ user/models.py
@@ -9,7 +9,12 @@ class userData(models.Model):
     user_phone = models.CharField(max_length=30)
     user_birthday = models.DateField()
     user_pic = models.ImageField(upload_to='images',blank=True)
-    # user_followers = models.ManyToManyField('self',related_name='followers',symmetrical=False,blank=True)
-    # user_following = models.ManyToManyField('self',related_name='following',symmetrical=False,blank=True)
     def __str__(self):
         return self.user_name
+    
+
+class userFollowers(models.Model):
+    following = models.ForeignKey(userData, on_delete=models.CASCADE,related_name="following",blank=True,default=1)
+    follower = models.ForeignKey(userData, on_delete=models.CASCADE,related_name='follower',blank=True)
+    def __str__(self):
+        return  self.following.user_name + " follows " + self.follower.user_name
